b8a86839a33ce936779b9e119b81b1eac9361496
KKDN7355 <KevinKDNguyen@gmail.com>
2025-02-19 22:05:19 +1100

    Initial commit

---
+++ API/Address Book/Controllers/ContactsController.cs
@@ -0,0 +1,62 @@
+﻿using Address_Book.Data;
+using Address_Book.Models;
+using Address_Book.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Address_Book.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly AddressBookDbContext dbContext;
+
+        public ContactsController(AddressBookDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            var contacts = dbContext.Contacts.ToList();
+            return Ok(contacts);
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(AddContactRequestDTO request)
+        {
+            var domainModelContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favourite = request.Favourite
+            };
+
+            dbContext.Contacts.Add(domainModelContact);
+            dbContext.SaveChanges();
+
+            return Ok(domainModelContact);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteContact(Guid id)
+        {
+            var contact = dbContext.Contacts.Find(id);
+
+            if (contact != null)
+            {
+                dbContext.Contacts.Remove(contact);
+                dbContext.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ API/Address Book/Data/AddressBookDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Address_Book.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Address_Book.Data
+{
+    public class AddressBookDbContext: DbContext
+    {
+        public AddressBookDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
